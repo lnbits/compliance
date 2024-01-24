38a3f3bbdc91709628e2fb70b8e2dcc2de1a0ea5
benarc <ben@arc.wales>
2024-01-24 10:33:49 +0000

    pointless

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 import asyncio
 
 from fastapi import APIRouter
-
+from lnbits.db import Database
 from lnbits.helpers import template_renderer
 from lnbits.tasks import catch_everything_and_restart
+
+# Pointless, but LNbits complains if there is no database.
+db = Database("ext_compliance")
 
 compliance_ext: APIRouter = APIRouter(
     prefix="/compliance", tags=["Compliance"]
@@ -18,7 +21,6 @@ temp_static_files = [
 
 def compliance_renderer():
     return template_renderer(["compliance/templates"])
-
 
 from .views import *
 from .views_api import *
